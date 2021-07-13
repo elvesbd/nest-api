85803e87be802b7939f680a7b8f8242a0fffc92d
Elves Brito <elvesbd@yahoo.com>
2021-07-13 10:54:39 -0300

    created resource routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TestController } from './test/test.controller';
+import { RoutesModule } from './routes/routes.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [RoutesModule],
+  controllers: [AppController, TestController],
   providers: [AppService],
 })
 export class AppModule {}
